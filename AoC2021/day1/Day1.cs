9f0b8663ebcffb4a4343acdef445960138ecb3fc
Dan Scott <Dan.Scott@sse.com>
2021-12-02 10:21:40 +0000

    one solution

---
+++ AoC2021/day1/Day1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2021
+{
+    public class Day1
+    {
+        public static void Run()
+        {
+            var readings = File.ReadAllText("day1/input.txt")
+                .Split("\r\n").Select((x, i) => new
+                {
+                    Value = int.Parse(x),
+                    Index = i
+                }).ToList();
+
+            var larger = readings
+                .Count(x => x.Index > 0 && x.Value > readings[x.Index - 1].Value);
+
+            Console.WriteLine($"{larger} readings are larger than the previous");
+        }
+    }
+}
